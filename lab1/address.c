5683410d545bd98d7a063813ef0f8c3bd2532ef9
Saranshichitra <Saranshi.chitra@gmail.com>
2025-09-14 14:29:14 +0530

    Add files via upload

---
+++ lab1/address.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("House No. 16\n");
+    printf("Chowki colony\n");
+    printf("Palampur distt Kangra,Himachal Pradesh \n");
+    printf("India\n");
+    return 0;
+}
